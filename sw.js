01d32ed106036acf7ac9d78a09e585abb1999ffd
東方彗楽園 <147727800+suirakuen@users.noreply.github.com>
2024-05-24 23:40:10 +0900

    TEST PWA

---
+++ sw.js
@@ -0,0 +1,22 @@
+var CACHE_NAME = 'pwa-caches';
+var urlsToCache = [
+    'suirakuen.github.io/index.html',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(caches
+        .open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches
+        .match(event.request)
+        .then(function(response) {
+            return response ? response : fetch(event.request);
+        })
+    );
+});
